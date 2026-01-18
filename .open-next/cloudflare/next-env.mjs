a868600c81cdafd71340a5d8a06142afcdfdf33c
magurh <magureanuhoria@gmail.com>
2026-01-18 22:27:12 +0000

    fix: add wrangler compatibility for cloudflare deploy

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"RESEND_API_KEY":"yre_frU8mmD7_GJ5gCxDiMA8QGNpMWwXmkWTT","CONTACT_TO_EMAIL":"magureanuhoria@gmail.com","CONTACT_FROM_EMAIL":"resonanceoracle@gmail.com"};
+export const development = {"RESEND_API_KEY":"yre_frU8mmD7_GJ5gCxDiMA8QGNpMWwXmkWTT","CONTACT_TO_EMAIL":"magureanuhoria@gmail.com","CONTACT_FROM_EMAIL":"resonanceoracle@gmail.com"};
+export const test = {"RESEND_API_KEY":"yre_frU8mmD7_GJ5gCxDiMA8QGNpMWwXmkWTT","CONTACT_TO_EMAIL":"magureanuhoria@gmail.com","CONTACT_FROM_EMAIL":"resonanceoracle@gmail.com"};
